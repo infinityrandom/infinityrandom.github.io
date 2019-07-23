6e963a6d9c716d8c2800d6ed8b4a7d621b4f3384
infinityrandom <53135868+infinityrandom@users.noreply.github.com>
2019-07-23 09:26:27 -1000

    redid some stuff

--- js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-
-    var myMenuButton = document.querySelector('.menu-button');
-    var myMainMenu = document.querySelector('header .site-nav');
-    myMenuButton.addEventListener('click', function () {
-        myMainMenu.classList.toggle('expanded');
-    });
-
-});
